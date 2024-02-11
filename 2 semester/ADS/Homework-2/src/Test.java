46145652760a5562445d2c87a4508a7104c52b72
klirik <wishreacher@gmail.com>
2024-02-11 23:05:37 +0200

    ии

---
+++ 2 semester/ADS/Homework-2/src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    static int smalltest(int i){
+
+        if (i%2==0) {return;}
+
+        else {return;}
+
+    }
+
+    public static void main(String[] args) {
+
+        int k = 2;
+
+        System.out.println(smalltest(k));
+
+    }
+}

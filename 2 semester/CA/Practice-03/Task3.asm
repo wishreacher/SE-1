099c2ab1150d22d5b63898160653b4588c011028
klirik <wishreacher@gmail.com>
2024-02-23 00:18:59 +0200

    [CA] пиздець

--- 2 semester/CA/Practice-03/Task3.asm
+++ 2 semester/CA/Practice-03/Task3.asm
@@ -1,15 +1,76 @@
-a: DW 19
-b: DB 0  
+a: db  "9"
+b: db 10
 
 start:
-mov ax, word a
+mov al, byte a 
+mov ah, byte b
+jmp firstPart
 
-; ділення на 8
-sar ax, 3; кожний зсув вправо ділить число на 2, тому робимо це 3 рази
-mov word a, ax
+firstPart:
+cmp al, 48
+jb secondPart
 
-; остача
-and ax, 7
-mov byte b, al
-mov bl, al;не впливає на логіку, просто щоб відобразити відповідь
-print reg
+cmp al, 57
+ja secondPart
+
+jmp outerIF
+
+secondPart:
+cmp al, 97
+jb thirdPart
+
+cmp al, 102
+ja thirdPart 
+
+cmp ah, 10
+jbe thirdPart
+
+jmp outerIF
+
+thirdPart:
+cmp ah, 10
+jbe outerELSE
+
+cmp al, 65
+jb outerELSE
+
+cmp al, 70
+ja outerELSE
+
+jmp outerIF
+
+
+outerIF:
+sub al, 48
+
+jmp innerIF
+
+
+outerELSE:
+mov al, -1
+jmp end
+
+innerIF:
+cmp al, 9
+jbe innerELSE
+
+cmp al, 23
+jge innerELSE
+
+sub al, 7
+
+jmp end
+
+innerELSE:
+cmp al, 39
+jbe end
+
+sub al, 39
+jmp end
+
+jmp end
+
+end:
+mov ah, 0x1
+mov byte a, al
+int 0x21

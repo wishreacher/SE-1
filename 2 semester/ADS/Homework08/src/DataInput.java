1ac237960c207af3ae50670de6366accc5971ffa
klirik <wishreacher@gmail.com>
2024-03-18 21:50:41 +0200

    нанана

---
+++ 2 semester/ADS/Homework08/src/DataInput.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public final class DataInput {
+    public static Long getLong(){
+        while(true) {
+            try {
+                String s = getString();
+                Long value = Long.valueOf(s);
+                return value;
+            } catch(IOException | NumberFormatException e) {
+                System.out.println("Wrong input! Please enter a valid long integer.");
+            }
+        }
+    }
+
+    public static char getChar() throws IOException{
+        while(true){
+            String s = getString();
+            if(s.length() == 1){
+                return s.charAt(0);
+            } else{
+                System.out.println("Wrong input. Please enter one character");
+            }
+
+        }
+    }
+
+    public static Integer getInt(){
+        while(true) {
+            try {
+                String s = getString();
+                Integer value = Integer.valueOf(s);
+                return value;
+            } catch(IOException | NumberFormatException e) {
+                System.out.println("Wrong input! Please enter a valid integer.");
+            }
+        }
+    }
+
+    public static String getString() throws IOException{
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String s = br.readLine();
+        return s;
+    }
+}
---
+++ 2 semester/ADS/Homework08/src/Main.java
@@ -0,0 +1,74 @@
+/*
+    Написати програму, що зчитує інформацію з текстового файлу і рахує кількість слів в файлі,
+    кількість унікальних слів в файлі, виводить інформацію про кількість слів, кількість
+    унікальних слів, унікальні слова за алфавітом і поруч скільки разів це слово зустрілося.
+    Унікальні слова - це, так би мовити, словарний запас файлу. Тобто, якщо у вас у файлі сто слів,
+    але при цьому там двадцять п'ять різних слів, кожне з яких повторюється чотири рази, то унікальних
+    слів - 25.
+
+    Продумати ієрархію класів.
+ */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Word {
+    String text;
+    int count;
+
+    Word(String text) {
+        this.text = text;
+        this.count = 1;
+    }
+
+    void incrementCount() {
+        this.count++;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        List<Word> words = new ArrayList<>();
+        int totalWords = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("test.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                StringTokenizer st = new StringTokenizer(line);
+                while (st.hasMoreTokens()) {
+                    String wordText = st.nextToken();
+                    totalWords++;
+                    Word word = findWord(words, wordText);
+                    if (word == null) {
+                        words.add(new Word(wordText));
+                    } else {
+                        word.incrementCount();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Collections.sort(words, (w1, w2) -> w1.text.compareTo(w2.text));
+
+        System.out.println("Total words: " + totalWords);
+        System.out.println("Unique words: " + words.size());
+        for (Word word : words) {
+            System.out.println(word.text + ": " + word.count);
+        }
+    }
+
+    private static Word findWord(List<Word> words, String text) {
+        for (Word word : words) {
+            if (word.text.equals(text)) {
+                return word;
+            }
+        }
+        return null;
+    }
+}
